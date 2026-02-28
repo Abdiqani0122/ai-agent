8cb73021922dea9b8fcc65b35e3e87ca549ca314
Abdiqani Abdullahi <abdiqaniabdullahi@gmail.com>
2026-02-28 12:32:40 +0100

    Added two more answer_questions

--- agent/tools.py
+++ agent/tools.py
@@ -4,4 +4,9 @@ def summarize_text(text: str) -> str:
 def answer_question(text: str) -> str:
     if "capital of norway" in text.lower():
         return "The capital of Norway is Oslo."
+    if "capital of sweden" in text:
+        return "The capital of Sweden is Stockholm."
+    if "capital of spain" in text:
+        return "The capital of Spain is Madrid."
+    
     return "I don't know the answer yet."
